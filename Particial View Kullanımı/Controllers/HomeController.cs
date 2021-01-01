957b7d210014aed27c77a4a1ad75bf2caf9881d9
Habibe Beggi <71319610+habibebeggi@users.noreply.github.com>
2021-01-02 00:37:51 +0300

    first commit

---
+++ Particial View Kullanımı/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Particial_View_Kullanımı.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Particial_View_Kullanımı.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public ActionResult Login(Models.User veri)
+        {
+            var data = new Models.User();
+            data.Ad = "Habibe";
+            data.Soyad = "Beggi";
+            data.Yas = 19;
+            data.Meslek = "Ogrenci";
+            return View(data);
+        }
+        
+    }
+}
---
+++ Particial View Kullanımı/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Particial_View_Kullanımı.Models
+{
+    public class User
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int Yas { get; set; }
+        public string Meslek { get; set; }
+    }
+}
